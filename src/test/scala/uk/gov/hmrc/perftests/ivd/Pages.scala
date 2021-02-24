aec10ddc79b2e6b2b1a7bd4dab6ee4b544ee45a7
dannybassi <72074461+dannybassi@users.noreply.github.com>
2021-02-24 16:50:35 +0000

    refactor

--- src/test/scala/uk/gov/hmrc/perftests/ivd/Pages.scala
+++ src/test/scala/uk/gov/hmrc/perftests/ivd/Pages.scala
@@ -67,7 +67,7 @@ trait Pages extends BaseRequests {
   object EnterBox67ValuePage extends Page("/disclosure/box-67-other-charges-deductions-amendment", "Enter Box 67")
   object EnterBox68ValuePage extends Page("/disclosure/box-68-adjustment-vat-value-amendment", "Enter Box 68")
   object UnderpaymentBoxConfirmationPage extends Page("/disclosure/confirm-reason-details", "Box Underpayment Confirmation")
-  object BoxReasonSummaryPage extends Page("/disclosure/underpayment-reason-summary", "Box Reason Summary Page")
-  object HasFurtherInformationPage extends Page("/disclosure/provide-information-option", "Have Further Information Page")
+  object BoxReasonSummaryPage extends Page("/disclosure/underpayment-reason-summary", "Box Reason Summary")
+  object HasFurtherInformationPage extends Page("/disclosure/provide-information-option", "Have Further Information")
 
 }
