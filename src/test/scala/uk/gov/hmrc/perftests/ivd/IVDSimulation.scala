77cb158065c902c50edc97b1c4119b8387489da5
Charlie Ashton <19330235+charlieashton@users.noreply.github.com>
2021-03-29 17:21:48 +0100

    added import vat dan flow (#59)

--- src/test/scala/uk/gov/hmrc/perftests/ivd/IVDSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/ivd/IVDSimulation.scala
@@ -239,8 +239,10 @@ class IVDSimulation extends PerformanceTestRunner {
       getRepresentativeDAN,
       postRepresentativeDAN,
       getRepresentativeDANDuty,
-      postRepresentativeDANDuty
-  )
+      postRepresentativeDANDuty,
+      getRepresentativeDANImportVAT,
+      postRepresentativeDANImportVAT
+    )
 
   runSimulation()
 }
