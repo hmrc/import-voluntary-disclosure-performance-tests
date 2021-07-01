9cb2bee57ee9066dbf53f64bd11091b375debed0
Jacek Malec <86240174+jacemale-hmrc@users.noreply.github.com>
2021-07-01 14:37:51 +0100

    Fix page reference

--- src/test/scala/uk/gov/hmrc/perftests/ivd/IVDRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/ivd/IVDRequests.scala
@@ -191,7 +191,7 @@ object IVDRequests extends BaseRequests with Pages {
   val getSendMoreDocumentation: HttpRequestBuilder = buildGetRequest(DoYouWantToSendMoreDocumentationPage)
   val postSendMoreDocumentation: HttpRequestBuilder = buildPostRequest(DoYouWantToSendMoreDocumentationPage, Some(List(("value", "true"))))
 
-  val getUploadSupportingDocumentation: HttpRequestBuilder = buildGetRequest(UploadSupportingDocumentationPollingPage,false)
+  val getUploadSupportingDocumentation: HttpRequestBuilder = buildGetRequest(UploadSupportingDocumentationPage,false)
   val getUploadSupportingDocumentationSuccessRedirect: HttpRequestBuilder = buildGetRequest(UploadSupportingDocumentationSuccessRedirectPage, false, expectedStatus = 303)
   val getUploadSupportingDocumentationPolling: HttpRequestBuilder =  buildGetRequest(UploadSupportingDocumentationPollingPage, false, expectedStatus = 303)
 
